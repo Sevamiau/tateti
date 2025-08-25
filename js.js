f762779f1c2a7ef7f7c35fe3b30792123d258cdb
¨Seva <sevakunjadas.bms@gmail.com>
2025-08-24 22:21:34 -0300

    feat: func stored in array pchoice

--- js.js
+++ js.js
@@ -1,7 +1,26 @@
-const buttons = document.querySelector('#btns');
-
-buttons.addEventListener('click', () => {
-    console.log(buttons.textContent)
-})
+const buttons = document.querySelectorAll('button');
+let pChoice = [];
 
 
+
+function playerChoice(){
+    for (let i = 0; i < buttons.length; i ++) {
+    buttons[i].addEventListener('click', () => {
+        console.log(buttons[i].textContent);
+        const selectedChoice = buttons[i].textContent;
+        pChoice.push(selectedChoice);
+        });
+    }
+}
+playerChoice();
+console.log(pChoice);
+
+function defineWinner() {
+    if (pChoice.length == 2) {
+        console.log('winner')
+    } else {
+        // console.log('whatever');
+    }
+}
+
+defineWinner()
